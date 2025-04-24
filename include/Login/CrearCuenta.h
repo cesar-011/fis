38b1afbcaccb5000df3949b7635f27548737a0df
Cesar <alu0101621302@ull.edu.es>
2025-04-24 14:35:54 +0000

    prueba

--- include/Login/CrearCuenta.h
+++ include/Login/CrearCuenta.h
@@ -1,6 +1,17 @@
 #ifndef CREARCUENTA_H_
 #define CREARCUENTA_H_
 
+#include "../User/Usuario.h"
+#include "../HashTable/HashTable.h"
 
+#include <fstream>
 
-#endif //CREARCUENTA_H_
+class CrearCuenta {
+ public:
+  CrearCuenta (const Usuario& user) : user_(user) {}
+  void AñadirCuenta(HashTable<Uid>&);
+ private:
+  Usuario user_;
+};
+
+#endif //CREARCUENTA_H_const
--- src/CrearCuenta.cc
+++ src/CrearCuenta.cc
@@ -1,1 +1,9 @@
-#include "../include/CrearCuenta.h"
+#include "../include/Login/CrearCuenta.h"
+
+
+void CrearCuenta::AñadirCuenta(HashTable<Uid>& base_datos) {
+  //Buscar si el uid ya exite, en ese caso, no añadirla ya que ya está añadida
+  if (base_datos.search(this->user_.get_uid())) {
+    
+  }
+}
